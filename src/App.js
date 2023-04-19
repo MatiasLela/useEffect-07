d112c251ace8b32bfca7c3b43e67420694806ed2
MatiasLela <matiaslela0@gmail.com>
2023-04-19 13:28:10 -0300

    Contadores

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { TextField } from '@mui/material';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import "./App.css";
 
 function App() {
+  const [texto, setTexto] = useState('');
+  const [cPalabras, setCPalabras] = useState(0);
+  const [cCaracteres, setCCaracteres] = useState(0);
+
+  useEffect(() =>{
+    const palabras = texto.split(' ');
+    setCPalabras(palabras.filter(palabra => palabra !== '').length);
+    setCCaracteres(texto.length);
+
+    if(texto.length >= 100){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Has alcanzado el limite de caracteres!!',
+      })
+    }
+  }, [texto]);
+
+  const handleCambioTexto = (e) => {
+    e.preventDefault();
+    setTexto(e.target.value);
+  }
+
+  const tema = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={tema}><CssBaseline/>
+        <h1>Contador de caracteres y palabras</h1>
+        <TextField inputProps={{ maxLength: 100 }} id="outlined" type="text" variant="outlined" value={texto} onChange={handleCambioTexto} max={100} label="Escribir..."/>
+        <p>Cantidad de caracteres: {cCaracteres}</p>
+        <p>Cantidad de palabras: {cPalabras}</p>
+      </ThemeProvider>
     </div>
   );
 }
